7671840df1fb0913c35103568ff4a00431b1f7c4
Hamza Topcu <hxmzx49@gmail.com>
2022-01-09 15:52:34 +0300

    .

---
+++ kareselSayilar.java
@@ -0,0 +1,26 @@
+public class kareselSayilar {
+
+    public static void main(String[] args) {
+int pay=1;
+int payda=2;
+
+int i=0;
+while(i<6){
+
+System.out.print(pay+"/"+payda+"  ");
+pay=payda;
+payda=payda*payda;
+i++;
+
+
+
+}
+        
+        
+        
+        
+        
+        
+    }
+    
+}
